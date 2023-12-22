19b153ec9b3581bfbdb715652f0aab1fc1af94e9
diegorezm <dmourarezende@gmail.com>
2023-12-22 19:18:27 -0300

    styling Image component

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,13 +4,12 @@ type props = {
   data: ImageInterface
 }
 export default function Image({ data }: props) {
-
   const handleButtonClick = async (image: string) => {
     return await invoke("change_bg_image", { imgPath: image });
   };
   return (
-    <div className="w-full h-60 border-2 rounded border-purple-color hover:border-pink-color group">
-      <button className="relative  p-0 w-fit h-full hover:opacity-20 transition-all" onClick={() => handleButtonClick(data.relativePath)}>
+    <div className="w-full h-60 border-2 rounded border-[var(--purple-color)] hover:border-[var(--pink-color)] group">
+      <button className="relative p-0 w-fit h-full hover:opacity-20 transition-all" onClick={() => handleButtonClick(data.relativePath)}>
         <img src={data.path} alt={data.fileName} className="w-full h-full object-cover" />
       </button>
     </div>
