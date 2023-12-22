a26b7c435f9f6e92a3990cd5a7c7efe93e95e8d0
diegorezm <dmourarezende@gmail.com>
2023-12-22 19:17:43 -0300

    stored path

--- src/hooks/useImages.ts
+++ src/hooks/useImages.ts
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 import { ImageQuery } from "../interfaces";
-import { getCachedData } from "../utils";
+import { getCachedData, getStoredPath } from "../utils";
 
 export const useImages = () => {
   const [imageQuery, setImageQuery] = useState<ImageQuery>({
     images: [],
     filtered: [],
-    search: ""
+    search: "",
+    defaultDir: ""
   });
 
   useEffect(() => {
     async function getImageQuery() {
       try {
         const imageCache = await getCachedData();
-        setImageQuery(query => ({ ...query, images: imageCache }));
+        const storedPath = getStoredPath();
+        setImageQuery(query => ({ ...query, images: imageCache, defaultDir: storedPath }));
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     }
-    getImageQuery(); 
-  }, []); 
+    getImageQuery();
+  }, []);
 
-  return {imageQuery, setImageQuery};
+  return { imageQuery, setImageQuery };
 };
 
