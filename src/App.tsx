e9800dfaa737baa8812212b5c8ed71fa37813d4b
diegorezm <dmourarezende@gmail.com>
2023-12-22 19:19:03 -0300

    main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-function App() {
+import Grid from "./components/Grid";
+import Navbar from "./components/Navbar";
+import { useImages } from "./hooks/useImages";
+export default function App() {
+  const { imageQuery, setImageQuery } = useImages();
   return (
-    <div className="container">
-    </div>
+    <main className="w-full flex flex-col justify-center items-center p-2">
+      <section className="w-full">
+        <Navbar imageQuery={imageQuery} setImageQuery={setImageQuery} />
+      </section>
+      <section className="flex flex-col items-center justify-center m-2 border-2 border-[var(--pink-color)] rounded">
+        <Grid imageQuery={imageQuery} />
+      </section>
+    </main>
   );
 }
 
-export default App;
