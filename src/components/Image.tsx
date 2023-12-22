7348ae383f47d0694a8ce1dd2f6e5dbf777e7e0e
diegorezm <dmourarezende@gmail.com>
2023-12-22 14:03:46 -0300

    image component

---
+++ src/components/Image.tsx
@@ -0,0 +1,18 @@
+import { invoke } from "@tauri-apps/api";
+import { ImageInterface } from "../interfaces";
+type props = {
+  data: ImageInterface
+}
+export default function Image({ data }: props) {
+
+  const handleButtonClick = async (image: string) => {
+    return await invoke("change_bg_image", { imgPath: image });
+  };
+  return (
+    <div className="w-full h-60 border-2 rounded border-purple-color hover:border-pink-color group">
+      <button className="relative  p-0 w-fit h-full hover:opacity-20 transition-all" onClick={() => handleButtonClick(data.relativePath)}>
+        <img src={data.path} alt={data.fileName} className="w-full h-full object-cover" />
+      </button>
+    </div>
+  );
+}
