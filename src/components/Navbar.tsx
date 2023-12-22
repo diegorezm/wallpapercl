7358ff4bacd56eb9a9d1b6403fa8d6dff606ab25
diegorezm <dmourarezende@gmail.com>
2023-12-22 19:40:34 -0300

    navbar working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,9 @@
-import { ImageQuery } from "../interfaces";
+
+import { IoFolderOpenOutline, IoRefresh } from "react-icons/io5";
+import { open } from "@tauri-apps/api/dialog";
+import { ImageInterface, ImageQuery } from "../interfaces";
 import Search from "./Search";
+import { getImagesFromDirectory } from "../utils";
 
 interface props {
   imageQuery: ImageQuery,
@@ -7,9 +11,35 @@ interface props {
 }
 
 export default function Navbar(props: props) {
+  const { imageQuery, setImageQuery } = props;
+  const handleRefreshButton = async () => {
+    const baseDir = imageQuery.defaultDir;
+    const images = await getImagesFromDirectory(baseDir);
+    setImageQuery(query => ({ ...query, images }));
+    window.location.reload();
+  };
+  const handleDirectoryClick = async () => {
+    const selected = (await open({
+      directory: true,
+      multiple: false,
+    })) as string;
+    imageQuery.defaultDir = selected;
+    const images = await getImagesFromDirectory(imageQuery.defaultDir) as ImageInterface[];
+    setImageQuery(query => ({...query, images}));
+  };
   return (
     <nav className="flex flex-row justify-between items-center px-2 w-full relative">
+      <button className="bg-gradient-to-b flex justify-center items-center from-[var(--pink-color)] to-[var(--purple-color)] w-[12%] md:w-[8%] lg:w-[5%] h-8 px-2 py-2 text-[var(--crust)] rounded" onClick={handleDirectoryClick}>
+        <span className="text-2xl text-center">
+          <IoFolderOpenOutline />
+        </span>
+      </button>
       <Search {...props} />
+      <button className="bg-gradient-to-b flex justify-center items-center from-[var(--pink-color)] to-[var(--purple-color)] w-[12%] md:w-[8%] lg:w-[5%] h-8 px-2 py-2 text-[var(--crust)] rounded" onClick={handleRefreshButton}>
+        <span className="text-2xl text-center">
+          <IoRefresh />
+        </span>
+      </button>
     </nav>
   );
 }
