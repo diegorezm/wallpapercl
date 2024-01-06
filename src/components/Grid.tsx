2b94b4772e1523a29ff3d83dfddb262a91452f8b
diegorezm <dmourarezende@gmail.com>
2024-01-05 23:46:51 -0300

    testing api query

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,8 +1,18 @@
-import { ImageQuery } from "../interfaces";
+import { useEffect, useState } from "react";
+import { ImageInterface, ImageQuery } from "../interfaces";
+import { convertApiImages } from "../utils";
 import Display from "./Display";
 export default function Grid({ imageQuery }: {
   imageQuery: ImageQuery
 }) {
+  const [imageQueryAPI, setImageQueryApi] = useState<ImageInterface[]>([]);
+  const imageQueryApi = async () => {
+    const data = await convertApiImages();
+    setImageQueryApi(data);
+  };
+  useEffect(() => {
+    imageQueryApi();
+  }, []);
   return (
     <>
       {imageQuery.search.trim().length > 0 && imageQuery.filtered.length < 1 && <div className="flex justify-center items-center text-center text-[var(--red-color)] text-2xl font-bold h-full w-full p-2">NO RESULTS FOUND!</div>}
