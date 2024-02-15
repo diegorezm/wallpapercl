d5ccb53eb4ea1cdc4161266d291e0cb583ad9973
diegorezm <dmourarezende@gmail.com>
2024-02-14 21:04:47 -0300

    actions static methods

---
+++ src/class/Actions.ts
@@ -0,0 +1,24 @@
+import { open } from "@tauri-apps/api/dialog"
+import { getImagesFromDirectory, getStoredPath, setCacheData, setStoredPath } from "../utils";
+
+export class Actions {
+  static handleRefreshButton = async () => {
+    const path = getStoredPath()
+    const images = await getImagesFromDirectory(path)
+    setCacheData({
+      images_directory: path,
+      images
+    })
+    return images
+  }
+
+  static handleDirectoryButton = async () => {
+    const selected = (await open({
+      directory: true,
+      multiple: false,
+    })) as string;
+    setStoredPath(selected)
+    return selected
+  }
+
+}
