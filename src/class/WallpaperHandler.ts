8fda1622737af277d143ab4236f550e22941a020
diegorezm <dmourarezende@gmail.com>
2024-02-14 21:04:41 -0300

    methods for handling the wallpaper

---
+++ src/class/WallpaperHandler.ts
@@ -0,0 +1,62 @@
+import { invoke } from "@tauri-apps/api"
+import { ImageInterface } from "../types"
+import { getCachedData, getImagesFromDirectory } from "../utils"
+
+export class WallpaperHandler {
+  private wallpaperTemplate: HTMLDivElement
+  private wallpaperContainer: HTMLDivElement
+  private images: ImageInterface[] = []
+  private _filteredImages: typeof this.images = []
+
+  public set setImages(new_images: ImageInterface[]) {
+    this.images = new_images
+    this.renderImages()
+  }
+
+  public get getImages() {
+    return this.images
+  }
+
+
+  public get filteredImages(): typeof this.images {
+    return this._filteredImages
+  }
+  public set filteredImages(value: typeof this.images) {
+    this._filteredImages = value
+  }
+
+  constructor() {
+    this.wallpaperTemplate = document.querySelector("[wallpaper-template]") as HTMLDivElement
+    this.wallpaperContainer = document.querySelector("[wallpaper-container]") as HTMLDivElement
+  }
+
+  public async loadImagesFromDirectory(path?: string) {
+    if (path) {
+      this.images = await getImagesFromDirectory(path)
+    } else {
+      this.images = await getCachedData()
+    }
+    this.renderImages()
+  }
+
+  public renderImages(imagesToRender: ImageInterface[] = this.images): void {
+    this.wallpaperContainer.innerHTML = '';
+    imagesToRender.forEach(image => {
+      const template = this.wallpaperTemplate.cloneNode(true) as HTMLDivElement;
+      const imageTag = template.querySelector("[wallpaper-image]") as HTMLImageElement;
+      imageTag.src = image.path;
+      imageTag.alt = image.fileName;
+      imageTag.onclick = async () => {
+        await invoke("change_wallpaper", { wallpaper: { file_path: image.relativePath, file_name: image.fileName } });
+      }
+      this.wallpaperContainer.append(template);
+    });
+  }
+
+  public searchHandler(searchQuery: string) {
+    const searchTerm = searchQuery.toLowerCase() || ""
+    this.filteredImages = this.images.filter((item) => item.fileName.toLowerCase().includes(searchTerm))
+    this.renderImages(this.filteredImages)
+  }
+
+}
