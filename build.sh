51364d8c660e3e06ee0813c352e83f2e7e68dcb3
diegorezm <dmourarezende@gmail.com>
2024-04-03 23:27:29 -0300

    better build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 ## doing this because th app image does not build automatically for some reason
 WORKDIR=./src-tauri/target/release/bundle/appimage/wallpapercl.AppDir/
-cp -i $WORKDIR/usr/bin/wallpapercl $HOME/.local/bin
+echo "tauri build..."
+npm run tauri build
+echo "Moving binary to local/bin..." 
+cp -i $WORKDIR/usr/bin/wallpapercl $HOME/.local/bin 
+echo "Creating appimage..." 
 appimagetool $WORKDIR
