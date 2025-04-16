cd4449122905dd3d935a8bb1339f34f25f2937f4
diegorezm <dmourarezende@gmail.com>
2025-04-15 23:46:39 -0300

    fixing xresources

--- apps/tui/src/theme.rs
+++ apps/tui/src/theme.rs
@@ -1,4 +1,4 @@
-use crate::xresources::get_xresource_color;
+use crate::xresources::get_xresource_colors;
 use ratatui::style::{Color, Modifier, Style};
 
 pub struct Theme {
@@ -12,18 +12,27 @@ pub struct Theme {
 
 impl Theme {
     pub fn new() -> Self {
-        let foreground =
-            get_xresource_color("wallpapercl.foreground").unwrap_or(Color::Rgb(186, 194, 222));
-        let background =
-            get_xresource_color("wallpapercl.background").unwrap_or(Color::Rgb(30, 30, 46));
+        let xr = get_xresource_colors();
+
+        let foreground_str = xr.get("wallpapercl.fg:");
+        let background_str = xr.get("wallpapercl.bg:");
+        let primary_str = xr.get("wallpapercl.pr:");
+        let subtext_str = xr.get("wallpapercl.sb:");
+        let red_str = xr.get("*color1");
+
+        // This feels wrong, but surely there will be no problems
+        let foreground = *foreground_str.unwrap_or(&Color::Rgb(186, 194, 222));
+        let background = *background_str.unwrap_or(&Color::Rgb(30, 30, 46));
+        let primary = *primary_str.unwrap_or(&Color::Rgb(203, 166, 247));
+        let subtext = *subtext_str.unwrap_or(&Color::Rgb(147, 153, 178));
+        let red = *red_str.unwrap_or(&Color::Rgb(243, 139, 168));
+
         Theme {
             foreground,
             background,
-            primary: get_xresource_color("wallpapercl.primary")
-                .unwrap_or(Color::Rgb(203, 166, 247)),
-            subtext: get_xresource_color("wallpapercl.subtext")
-                .unwrap_or(Color::Rgb(147, 153, 178)),
-            red: get_xresource_color("*color1").unwrap_or(Color::Rgb(243, 139, 168)),
+            primary,
+            subtext,
+            red,
             selected_style: Style::new()
                 .bg(foreground)
                 .fg(background)
--- apps/tui/src/xresources.rs
+++ apps/tui/src/xresources.rs
@@ -1,23 +1,32 @@
-use std::process::Command;
+use std::{collections::HashMap, process::Command};
 
 use ratatui::style::Color;
 
-pub fn get_xresource_color(color_name: &str) -> Option<Color> {
-    let output = Command::new("xrdb").arg("-query").output().ok()?;
+pub fn get_xresource_colors() -> HashMap<String, Color> {
+    let output_result = Command::new("xrdb").arg("-query").output();
+    let mut map: HashMap<String, Color> = HashMap::new();
 
-    if output.status.success() {
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        for line in stdout.lines() {
-            if line.starts_with(color_name) {
-                if let Some(color_value) = line.split_whitespace().last() {
-                    if let Ok(rgb) = hex_to_rgb(color_value) {
-                        return Some(Color::Rgb(rgb.0, rgb.1, rgb.2));
+    match output_result {
+        Ok(output) if output.status.success() => {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            for line in stdout.lines() {
+                let whitespaced_line: Vec<&str> = line.split_whitespace().collect();
+                if let Some(&key) = whitespaced_line.first() {
+                    if let Some(&color) = whitespaced_line.last() {
+                        if let Ok(rgb) = hex_to_rgb(color) {
+                            map.insert(key.to_string(), Color::Rgb(rgb.0, rgb.1, rgb.2));
+                        }
                     }
                 }
             }
+            map
+        }
+        Ok(_) => HashMap::new(),
+        Err(e) => {
+            eprintln!("Error executing xrdb: {}", e);
+            return HashMap::new();
         }
     }
-    None
 }
 
 fn hex_to_rgb(hex: &str) -> Result<(u8, u8, u8), String> {
